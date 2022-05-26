df269c8d0a542a73fa49533c48263dddf9b3a093
Fabian Wosar <fabian.wosar@emsisoft.com>
2022-05-27 00:17:26 +0100

    Added proof of concept

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# unintellisense
+# Proof of concept
+
+Demonstration of Visual Studio Intellisense bug in combination with target_precompile_headers.
---
+++ src/unintellisense.cpp
@@ -0,0 +1,13 @@
+﻿int main()
+{
+	std::string compiler;
+
+#ifdef __clang__
+	compiler = "Clang";
+#else
+	compiler = "MSVC";
+#endif
+
+	std::cout << "Hello from " << compiler << "!" << std::endl;
+	return 0;
+}
